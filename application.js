9cd053901583159c351891ca6ab6849fe9cb4788
Hesham Elrehaimy <hesham.elrehaimy@3won.com>
2019-10-26 13:51:50 -0500

    Use array instead.

--- application.js
+++ application.js
@@ -5,31 +5,56 @@ window.onload = function() {
 
 var printTemplate = function(event) {
   event.preventDefault()
-  var code = this.aymanscode.value
-  var outputDiv = document.querySelector('.output')
+  //var code = this.aymanscode.value
+  //var outputDiv = document.querySelector('.output')
+  //
+  //var template = "\nCOMPUTE ECMODAY=0.";
+  //
+  //  for(var h = 0; h <= 125; h++) {
+  //    for(var j = 1; j <= 15; j++) {
+  //      template += "\n\n";
+  //      template += "IF ((PR" + j + " = " + code + ") & (PRDAY" + j + " =" + h + ")) ECMODAY=" + h + ".";
+  //    }
+  //  }
+  //
+  //  for(var hj = 1; hj <= 15; hj++) {
+  //    template += "\n\n";
+  //    template += "IF ((PR" + hj + " = " + code + ") & (PRDAY" + hj + " >125)) ECMODAY=999.";
+  //  }
+  //
+  //  template += "\n\n";
+  //  template += "EXECUTE.\n\n";
+  //  template += "COMPUTE NO_ECMO_DAY_ZERO=ECMO = 0 & ECMODAY = 0.\n\n";
+  //  template += "EXECUTE.\n\n";
+  //  template += "COMPUTE ECMO_DAY_ZERO_REAL=ECMO = 1 & ECMODAY = 0.\n\n";
+  //  template += "EXECUTE.\n\n";
+  //  template += "COMPUTE LOS_AFTER_ECMO=LOS - ECMODAY.\n\n";
+  //  template += "EXECUTE.\n\n";
+  //
+  //outputDiv.innerText = template;
   
-  var template = "\nCOMPUTE ECMODAY=0.";
-
-    for(var h = 0; h <= 125; h++) {
-      for(var j = 1; j <= 15; j++) {
-        template += "\n\n";
-        template += "IF ((PR" + j + " = " + code + ") & (PRDAY" + j + " =" + h + ")) ECMODAY=" + h + ".";
-      }
-    }
-
-    for(var hj = 1; hj <= 15; hj++) {
-      template += "\n\n";
-      template += "IF ((PR" + hj + " = " + code + ") & (PRDAY" + hj + " >125)) ECMODAY=999.";
-    }
-
-    template += "\n\n";
-    template += "EXECUTE.\n\n";
-    template += "COMPUTE NO_ECMO_DAY_ZERO=ECMO = 0 & ECMODAY = 0.\n\n";
-    template += "EXECUTE.\n\n";
-    template += "COMPUTE ECMO_DAY_ZERO_REAL=ECMO = 1 & ECMODAY = 0.\n\n";
-    template += "EXECUTE.\n\n";
-    template += "COMPUTE LOS_AFTER_ECMO=LOS - ECMODAY.\n\n";
-    template += "EXECUTE.\n\n";
+  var bigTemplate = "";
   
-  outputDiv.innerText = template;
+  var codesString = this.aymanscode.value;
+  var codesArray = codesString.split(" ");
+	for(var jh=0; jh < codes.length; jh++) {
+	  var code = codes[jh];
+	  var template = "COMPUTE CABGDAY=0.";
+
+		for(var h = 0; h <= 125; h++) {
+		  for(var j = 1; j <= 15; j++) {
+			template += "\n";
+			template += 'IF ((I10_PR' + j + ' = "' + code + '") & (PRDAY' + j + ' =' + h + ')) CABGDAY=' + h + '.';
+		  }
+		}
+
+		for(var hj = 1; hj <= 15; hj++) {
+		  template += "\n";
+		  template += 'IF ((I10_PR' + hj + ' = "' + code + '") & (PRDAY' + hj + ' >125)) CABGDAY=999.';
+		}
+
+		template += "\n";
+		
+		bigTemplate += template;
+	} 
 }
